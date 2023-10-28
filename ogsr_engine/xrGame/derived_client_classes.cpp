bb5e228fe5d58151446e02c0ad9097ff7f648015
xer-urg <alex.narrator@gmail.com>
2023-10-28 15:44:32 +0300

    сериптоекспорт

--- ogsr_engine/xrGame/derived_client_classes.cpp
+++ ogsr_engine/xrGame/derived_client_classes.cpp
@@ -80,7 +80,9 @@ void CEatableItemScript::script_register(lua_State* L)
     module(L)[class_<CEatableItem, CInventoryItem>("CEatableItem")
                   .def_readwrite("eat_portions_num", &CEatableItem::m_iPortionsNum)
                   .def_readwrite("eat_start_portions_num", &CEatableItem::m_iStartPortionsNum)
-                  .def_readwrite("can_be_eaten", &CEatableItem::m_bCanBeEaten),
+                  .def_readwrite("can_be_eaten", &CEatableItem::m_bCanBeEaten)
+                  .def("get_influence", &CEatableItem::GetItemInfluence)
+                  .def("get_boost", &CEatableItem::GetItemBoost),
               class_<CEatableItemObject, bases<CEatableItem, CGameObject>>("CEatableItemObject")];
 }
 
