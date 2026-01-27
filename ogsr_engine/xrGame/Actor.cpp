b7ade69b033558abcdc10128cced0eaa0bc8c298
𝐒𝐈𝐌𝐏 𝐀𝐈 <16670637+xrSimpAI@users.noreply.github.com>
2026-01-27 18:37:03 +0300

    Another fix for actor's shadow
    addition to 028275682

--- ogsr_engine/xrGame/Actor.cpp
+++ ogsr_engine/xrGame/Actor.cpp
@@ -1121,8 +1121,8 @@ void CActor::shedule_Update(u32 DT)
     }
 
     //если в режиме HUD, то сама модель актера не рисуется
-    if (!character_physics_support()->IsRemoved() && !m_holder)
-        setVisible(!HUDview());
+    const bool hud_view = m_holder ? m_holder->HUDView() : cam_active == eacFirstEye;
+    setVisible(!character_physics_support()->IsRemoved() && !hud_view);
 
     //что актер видит перед собой
     collide::rq_result& RQ = HUD().GetCurrentRayQuery();
@@ -1212,7 +1212,7 @@ void CActor::renderable_Render(u32 context_id, IRenderable* root)
     {
         inherited::renderable_Render(context_id, root);
 
-        if (!HUDview())
+        if (!m_holder && !character_physics_support()->IsRemoved())
         {
             CInventoryOwner::renderable_Render(context_id, root);
         }
@@ -1221,7 +1221,7 @@ void CActor::renderable_Render(u32 context_id, IRenderable* root)
 
 BOOL CActor::renderable_ShadowReceive()
 {
-    if (m_holder)
+    if (m_holder || character_physics_support()->IsRemoved())
         return FALSE;
 
     return TRUE;
